5ff8d201617ac0ac6af792cf44f31f9af38982a1
cubdesign <takeo@cubdesign.com>
2023-07-21 18:35:36 +0900

    add sshpass password

--- lib/middleman-deploy/methods/rsync.rb
+++ lib/middleman-deploy/methods/rsync.rb
@@ -2,7 +2,7 @@ module Middleman
   module Deploy
     module Methods
       class Rsync < Base
-        attr_reader :clean, :flags, :host, :path, :port, :user
+        attr_reader :clean, :flags, :host, :path, :port, :user, :password
 
         def initialize(server_instance, options = {})
           super(server_instance, options)
@@ -13,23 +13,26 @@ module Middleman
           @path   = self.options.path
           @port   = self.options.port
           @user   = self.options.user
+
+          @password = self.options.password
+
+
         end
 
         def process
-          # # Append "@" to user if provided.
-          # user      = "#{self.user}@" if self.user && !self.user.empty?
-          #
-          # dest_url  = "#{user}#{self.host}:#{self.path}"
-          # flags     = self.flags || '-avz'
-          # command   = "rsync #{flags} '-e ssh -p #{self.port}' #{self.server_instance.build_dir}/ #{dest_url}"
-          #
-          # if self.clean
-          #   command += ' --delete'
-          # end
-          #
-          # puts "## Deploying via rsync to #{dest_url} port=#{self.port}"
+          # Append "@" to user if provided.
+          user      = "#{self.user}@" if self.user && !self.user.empty?
 
-          puts "## test test test"
+          dest_url  = "#{user}#{self.host}:#{self.path}"
+          flags     = self.flags || '-avz'
+          command   = "rsync #{flags} '-e sshpass -p #{self.password}  ssh -p #{self.port}' #{self.server_instance.build_dir}/ #{dest_url}"
+
+          if self.clean
+            command += ' --delete'
+          end
+
+          puts "## Deploying via rsync to #{dest_url} port=#{self.port}"
+
           exec command
         end
       end
