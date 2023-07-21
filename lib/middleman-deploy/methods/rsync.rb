708b0ae8d68f610e106e12e2ff62f3a4dcb56f36
cubdesign <takeo@cubdesign.com>
2023-07-21 14:53:02 +0900

    remove rsync

--- lib/middleman-deploy/methods/rsync.rb
+++ lib/middleman-deploy/methods/rsync.rb
@@ -16,18 +16,20 @@ module Middleman
         end
 
         def process
-          # Append "@" to user if provided.
-          user      = "#{self.user}@" if self.user && !self.user.empty?
+          # # Append "@" to user if provided.
+          # user      = "#{self.user}@" if self.user && !self.user.empty?
+          #
+          # dest_url  = "#{user}#{self.host}:#{self.path}"
+          # flags     = self.flags || '-avz'
+          # command   = "rsync #{flags} '-e ssh -p #{self.port}' #{self.server_instance.build_dir}/ #{dest_url}"
+          #
+          # if self.clean
+          #   command += ' --delete'
+          # end
+          #
+          # puts "## Deploying via rsync to #{dest_url} port=#{self.port}"
 
-          dest_url  = "#{user}#{self.host}:#{self.path}"
-          flags     = self.flags || '-avz'
-          command   = "rsync #{flags} '-e ssh -p #{self.port}' #{self.server_instance.build_dir}/ #{dest_url}"
-
-          if self.clean
-            command += ' --delete'
-          end
-
-          puts "## Deploying via rsync to #{dest_url} port=#{self.port}"
+          puts "## test test test"
           exec command
         end
       end
